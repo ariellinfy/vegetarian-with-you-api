a1d7f21ee2d0c07656fd4b1ea0301867a5cac6e6
ariellinfy <72408793+ariellinfy@users.noreply.github.com>
2021-04-09 00:03:22 -0600

    added request user review handler

---
+++ reviews-handler/request-user-reviews.js
@@ -0,0 +1,26 @@
+const requestUserReviews = (knex) => async (req, res) => {
+    if (req.userId) {
+        try {
+            await knex.select('*')
+            .from('reviews')
+            .where('review_owner', '=', req.userId)
+            .orderBy('last_modified', 'desc')
+            .then(data => {
+                const token = refreshToken.refresh(req.exp, req.userId, req.token);
+                if (!token) {
+                    res.status(400).json('token expired');
+                }
+                return res.status(200).json({ data: data, token });
+            })
+            .catch(err => res.status(400).json({ error: 'unable to fetch data' }))
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    } else {
+        res.status(400).json({ error: 'authentication error' });
+    }
+};
+
+module.exports = {
+    requestUserReviews: requestUserReviews
+};
