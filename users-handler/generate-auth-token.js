a04d225a3db4b61b88bf6ec775cc995f34510d4f
ariellinfy <72408793+ariellinfy@users.noreply.github.com>
2021-05-12 13:48:42 -0600

    added env variable

--- users-handler/generate-auth-token.js
+++ users-handler/generate-auth-token.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const token = (payload) => jwt.sign({ id: payload.user_id }, 'process.env.JWT_SECRET', { expiresIn: '1h' });
+const token = (payload) => jwt.sign({ id: payload.user_id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
 module.exports = {
     token
