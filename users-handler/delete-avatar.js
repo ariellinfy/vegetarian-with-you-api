ce4f35b6b5de8a59e2f5ada914ac70f1fed2cc0e
ariellinfy <72408793+ariellinfy@users.noreply.github.com>
2021-05-24 14:43:21 -0600

    worked on avatar delete handler

--- users-handler/delete-avatar.js
+++ users-handler/delete-avatar.js
@@ -1,25 +1,38 @@
-const handleDeleteAvatar = (knex) => async (req, res) => {
+const cloudinary = require('cloudinary').v2;
+
+const handleDeleteAvatar = (knex) => (req, res) => {
     const { avatar } = req.body;
 
-    if (!avatar) {
+    if (!avatar.public_id) {
 		return res.status(400).json({ error: 'Avatar url missing.' });
 	};
 
-    try {
-        await knex('users').where({ user_id: req.userId })
-        .update({
-            avatar: null,
-            last_modified: new Date()
-        })
-        .returning('*')
-        .then(user => {
-            return res.status(200).json({ user: user[0] });
-        })
-        .catch(err => res.status(400).json({ error: 'Something wrong with fetching / updating user avatar, app under maintenance.' }))
-    } catch (e) {
-        console.log(e);
-        return res.status(400).json({ error: 'Fail to delete avatar, app under maintenance.' });
-    }
+    cloudinary.config({ 
+        cloud_name: 'alinfy', 
+        api_key: '225325956632848', 
+        api_secret: 'pNuy4D20wzTqjorV1y47ms_dKok' 
+    });
+
+    cloudinary.uploader.destroy(avatar.public_id, invalidate=true, function(error, result) {
+        if (error) {
+            return res.status(400).json({ error: 'Fail to remove cloudinary avatar, app under maintenance.' });
+        };
+        if (result.result === 'not found') {
+            return res.status(400).json({ error: 'Cloudinary avatar not found, app under maintenance.' });
+        };
+        if (result.result === 'ok') {
+            knex('users').where({ user_id: req.userId })
+            .update({
+                avatar: null,
+                last_modified: new Date()
+            })
+            .returning('*')
+            .then(user => {
+                return res.status(200).json({ user: user[0] });
+            })
+            .catch(err => res.status(400).json({ error: 'Something wrong with updating user avatar / fetching user info, app under maintenance.' }))
+        };
+    })
 };
 
 module.exports = {
